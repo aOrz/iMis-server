8cefbc70c94577a941cb91ba20fa4a1caaf943fd
wangzhen44 <rememberwz@gmail.com>
2017-10-21 20:57:55 +0800

    select

--- app/controller/api/v1/log.js
+++ app/controller/api/v1/log.js
@@ -4,7 +4,7 @@ module.exports = app => {
   class LogController extends app.Controller {
     async create() {
       const ctx = this.ctx;
-      let { type } = ctx.params;
+      const { type } = ctx.params;
       const { notice = false, logs = '', title = '' } = ctx.request.body;
       const { userid } = this.ctx.user;
       if (notice) {
@@ -18,6 +18,42 @@ module.exports = app => {
         title,
       });
       this.ctx.body = JSON.stringify(this.ctx.request.body);
+    }
+
+    async getLogBytype() {
+      const { ctx } = this;
+      const { user, params } = ctx;
+      const { userid } = user;
+
+      const { type, page = 0 } = params;
+      const where = { userid };
+      if (type) {
+        where.userid = userid;
+      }
+      const results = await this.app.mysql.select('logs', {
+        // 搜索 post 表
+        where, // WHERE 条件
+        columns: [ 'notice', 'title', 'logs', 'type', 'creat_time' ], // 要查询的表字段
+        orders: [[ 'creat_time', 'desc' ]], // 排序方式
+        limit: 20, // 返回数据量
+        offset: page, // 数据偏移量
+      });
+      this.ctx.body = {
+        code: 200,
+        data: results,
+      };
+    }
+
+    async getTypes() {
+      const { ctx } = this;
+      const { user } = ctx;
+      const { userid } = user;
+      const results = await this.app.mysql.query(`select \`type\`,count(*) as c from logs WHERE \`userid\` = ${userid} group by \`type\` order by c desc;
+      `);
+      this.ctx.body = {
+        code: 200,
+        data: results,
+      };
     }
   }
   return LogController;
--- app/router.js
+++ app/router.js
@@ -3,4 +3,8 @@
 module.exports = app => {
   app.get('/', 'home.index');
   app.post('/api/v1/log/:type', 'api.v1.log.create');
+
+  app.get('/api/v1/log/:page/:type', 'api.v1.log.getLogBytype');
+  app.get('/api/v1/log/:page', 'api.v1.log.getLogBytype');
+  app.get('/api/v1/get/log/types', 'api.v1.log.getTypes');
 };
