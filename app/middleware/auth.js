e07b52c3b8037cac83cd344d35f960e34cbe9d21
wangzhen44 <rememberwz@gmail.com>
2017-10-21 10:43:23 +0800

    login

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -8,6 +8,11 @@ module.exports = function auth() {
       options.server_token = token;
     } else {
       options.app_token = token;
+    } else {
+      ctx.body = {
+        code: 1,
+        error: 'login error,缺少token 或者 type',
+      };
     }
     const user = await ctx.app.mysql.get('user', options);
     if (token && user) {
